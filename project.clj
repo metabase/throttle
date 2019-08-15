579ce03950109c9807726ba4dfbd51d0e2a1cc58
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2019-08-15 02:28:22 +0200

    Update project.clj based on camsaul/clojure-library-template

--- project.clj
+++ project.clj
@@ -1,17 +1,72 @@
 (defproject metabase/throttle "1.0.1"
   :description "Simple tools for throttling API endpoints and other code."
   :url "https://github.com/metabase/throttle"
+  :min-lein-version "2.5.0"
+
   :license {:name "Lesser GPL"
             :url "https://www.gnu.org/licenses/lgpl.txt"}
-  :min-lein-version "2.5.0"
-  :dependencies [[org.clojure/math.numeric-tower "0.0.4"
-                  :exclusions [org.clojure/clojure]]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [expectations "2.1.4"]]
-                   :plugins [[lein-expectations "0.0.8"]
-                             [jonase/eastwood "0.2.3"]
-                             [lein-bikeshed "0.3.0"]]
-                   :eastwood {:add-linters [:unused-private-vars]}
-                   :aliases {"bikeshed" ["bikeshed" "--max-line-length" "160"]
-                             "test" ["expectations"]}}}
-  :deploy-repositories [["clojars" {:sign-releases false}]])
+
+  :aliases
+  {"test"                      ["with-profile" "+expectations" "expectations"]
+   "bikeshed"                  ["with-profile" "+bikeshed" "bikeshed" "--max-line-length" "120"]
+   "check-namespace-decls"     ["with-profile" "+check-namespace-decls" "check-namespace-decls"]
+   "eastwood"                  ["with-profile" "+eastwood" "eastwood"]
+   "check-reflection-warnings" ["with-profile" "+reflection-warnings" "check"]
+   "docstring-checker"         ["with-profile" "+docstring-checker" "docstring-checker"]
+   ;; `lein lint` will run all linters
+   "lint"                      ["do" ["eastwood"] ["bikeshed"] ["check-namespace-decls"] ["docstring-checker"]]}
+
+  :dependencies
+  [[org.clojure/math.numeric-tower "0.0.4" :exclusions [org.clojure/clojure]]]
+
+  :profiles
+  {:dev
+   {:dependencies
+    [[org.clojure/clojure "1.8.0"]
+     [expectations "2.1.4"]]
+
+    :injections
+    [(require 'expectations)
+     ((resolve 'expectations/disable-run-on-shutdown))]
+
+    :jvm-opts
+    ["-Xverify:none"]}
+
+   :expectations
+   {:plugins [[lein-expectations "0.0.8" :exclusions [expectations]]]}
+
+   :eastwood
+   {:plugins
+    [[jonase/eastwood "0.3.5" :exclusions [org.clojure/clojure]]]
+
+    :add-linters
+    [:unused-private-vars
+     :unused-namespaces
+     :unused-fn-args
+     :unused-locals]
+
+    :exclude-linters
+    [:deprecations]}
+
+   :docstring-checker
+   {:plugins
+    [[docstring-checker "1.0.3"]]
+
+    :docstring-checker
+    {:exclude [#"test"]}}
+
+   :bikeshed
+   {:plugins
+    [[lein-bikeshed "0.5.2"]]}
+
+   :check-namespace-decls
+   {:plugins               [[lein-check-namespace-decls "1.0.2"]]
+    :source-paths          ["test"]
+    :check-namespace-decls {:prefix-rewriting true}}}
+
+  :deploy-repositories
+  [["clojars"
+    {:url           "https://clojars.org/repo"
+     :username      :env/clojars_username
+     :password      :env/clojars_password
+     :sign-releases false}]])
